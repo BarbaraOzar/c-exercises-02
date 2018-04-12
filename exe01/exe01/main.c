9be2cbacb79945f73ed900eed683fb70e5c7903a
BarbaraOzar <barbara.ozar@gmail.com>
2018-04-12 10:48:48 +0200

    exe02

--- exe01/exe01/main.c
+++ exe01/exe01/main.c
@@ -2,14 +2,16 @@
  * exe01.c
  *
  * Created: 2018-04-12 09:28:46
- * Author : Dell
+ * Author : Barbara
  */ 
 
 #include <math.h>
 #include <stdio.h>
 
 int power(int *x, int *y);
-void multiSwap(int *x, int *y, int *z);
+void multi_swap(int *x, int *y, int *z);
+int sum_arrays(int *a, int *b, int size_a, int size_b);
+
 
 int main(void)
 {
@@ -17,8 +19,11 @@ int main(void)
 	
 	printf("value of a = %d, value of b = %d, value of c = %d\n", a, b, c);
     printf("a to the power of b = %d\n", power(&a, &b));
-    multiSwap(&a, &b, &c);
+    multi_swap(&a, &b, &c);
     printf("value of a = %d, value of b = %d, value of c = %d\n", a, b, c);
+    
+    int d[4] = {1, 2, 3, 4}, e[4] = {3, 2, 3, 1};
+    printf("total of both arrays = %d\n", sum_arrays(&d, &e, 4, 4));
     	
 }
 
@@ -27,10 +32,30 @@ int power(int *x, int *y)
 	return pow(*x, *y);
 }
 
-void multiSwap(int *x, int *y, int *z) 
+void multi_swap(int *x, int *y, int *z) 
 {
 	int temp = *x;
 	*x = *y;
 	*y = *z;
 	*z = temp;
+}
+
+int sum_arrays(int *a, int *b, int size_a, int size_b)
+{
+    int total;
+    int *pa = a, *pb = b;
+    
+    for(int i = 0; i < size_a; i++)
+    {
+        total += *pa;
+        pa++;
+    }
+    
+    for(int i = 0; i < size_b; i++)
+    {
+        total += *pb;
+        pb++;
+    }
+    
+    return total;
 }
